b6307650bfc6fa5c06674fefc4e2f113692dfe0b
Blake Imsland <blake@retroco.de>
2012-07-03 15:14:23 -0700

    Use ActiveSupport::Concern to handle including

--- lib/delegates_attributes_to.rb
+++ lib/delegates_attributes_to.rb
@@ -1,23 +1,22 @@
+require "active_support/concern"
+
 module DelegatesAttributesTo
+  extend ActiveSupport::Concern
 
   DEFAULT_REJECTED_COLUMNS = ['created_at','created_on','updated_at','updated_on','lock_version','type','id','position','parent_id','lft','rgt'].freeze
   DIRTY_SUFFIXES = ["_changed?", "_change", "_will_change!", "_was"].freeze
 
-  def self.included(base)
-    base.extend ClassMethods
-    base.send :include, InstanceMethods
+  included do
+    alias_method_chain :assign_multiparameter_attributes, :delegation
 
-    base.alias_method_chain :assign_multiparameter_attributes, :delegation
+    class_attribute :default_rejected_delegate_columns
+    self.default_rejected_delegate_columns = DEFAULT_REJECTED_COLUMNS.dup
 
-    base.class_attribute :default_rejected_delegate_columns
-    base.default_rejected_delegate_columns = DEFAULT_REJECTED_COLUMNS.dup
-
-    base.class_attribute :delegated_attributes
-    base.delegated_attributes = HashWithIndifferentAccess.new
+    class_attribute :delegated_attributes
+    self.delegated_attributes = HashWithIndifferentAccess.new
   end
 
   module ClassMethods
-
     ATTRIBUTE_SUFFIXES = (['', '=', '?'] + DIRTY_SUFFIXES).freeze
 
     # has_one :profile
@@ -73,71 +72,68 @@ module DelegatesAttributesTo
 
     private
 
-      def delegate_association(macro, association, *args)
-        options = args.extract_options!
-        # assosiation reflection doesn't ignore prefix option and raises ArgumentError
-        prefix = options.delete(:prefix)
-        send(macro, association, options) unless reflect_on_association(association)
-        options[:to] = association
-        # give back prefix option
-        options[:prefix] = prefix
-        args << options
-        delegate_attributes(*args)
-      end
+    def delegate_association(macro, association, *args)
+      options = args.extract_options!
+      # assosiation reflection doesn't ignore prefix option and raises ArgumentError
+      prefix = options.delete(:prefix)
+      send(macro, association, options) unless reflect_on_association(association)
+      options[:to] = association
+      # give back prefix option
+      options[:prefix] = prefix
+      args << options
+      delegate_attributes(*args)
+    end
   end
 
-  module InstanceMethods
+  private
 
-    private
+  def assign_multiparameter_attributes_with_delegation(pairs)
+    delegated_pairs = {}
+    original_pairs  = []
 
-      def assign_multiparameter_attributes_with_delegation(pairs)
-        delegated_pairs = {}
-        original_pairs  = []
+    pairs.each do |name, value|
+      # it splits multiparameter attribute
+      # 'published_at(2i)'  => ['published_at(2i)', 'published_at', '(2i)']
+      # 'published_at'      => ['published_at',     'published_at',  nil  ]
+      __, delegated_attribute, suffix = name.match(/^(\w+)(\([0-9]*\w\))?$/).to_a
+      association, attribute = self.class.delegated_attributes[delegated_attribute]
 
-        pairs.each do |name, value|
-          # it splits multiparameter attribute
-          # 'published_at(2i)'  => ['published_at(2i)', 'published_at', '(2i)']
-          # 'published_at'      => ['published_at',     'published_at',  nil  ]
-          __, delegated_attribute, suffix = name.match(/^(\w+)(\([0-9]*\w\))?$/).to_a
-          association, attribute = self.class.delegated_attributes[delegated_attribute]
-
-          if association
-            (delegated_pairs[association] ||= {})["#{attribute}#{suffix}"] = value
-          else
-            original_pairs << [name, value]
-          end
-        end
-
-        delegated_pairs.each do |association, attributes|
-          association_object = send(association) || send("build_#{association}")
-          # let association_object handle its multiparameter attributes
-          association_object.attributes = attributes
-        end
-
-        assign_multiparameter_attributes_without_delegation(original_pairs)
+      if association
+        (delegated_pairs[association] ||= {})["#{attribute}#{suffix}"] = value
+      else
+        original_pairs << [name, value]
       end
+    end
 
-      def changed_attributes
-        result = {}
-        self.class.delegated_attributes.each do |delegated_attribute, (association, attribute)|
-          # If an association isn't loaded it hasn't changed at all. So we skip it.
-          # If we don't skip it and have mutual delegation beetween 2 models
-          # we get SystemStackError: stack level too deep while trying to load
-          # a chain like user.profile.user.profile.user.profile...
-          next unless send(:association, association).loaded?
-          # skip if association object is nil
-          next unless association_object = send(association)
-          # call private method #changed_attributes
-          association_changed_attributes = association_object.send(:changed_attributes)
-          # next if attribute hasn't been changed
-          next unless association_changed_attributes.has_key?(attribute.to_s)
+    delegated_pairs.each do |association, attributes|
+      association_object = send(association) || send("build_#{association}")
+      # let association_object handle its multiparameter attributes
+      association_object.attributes = attributes
+    end
 
-          result.merge! delegated_attribute => association_changed_attributes[attribute]
-        end
-        changed_attributes = super
-        changed_attributes.merge!(result)
-        changed_attributes
-      end
+    assign_multiparameter_attributes_without_delegation(original_pairs)
+  end
+
+  def changed_attributes
+    result = {}
+    self.class.delegated_attributes.each do |delegated_attribute, (association, attribute)|
+      # If an association isn't loaded it hasn't changed at all. So we skip it.
+      # If we don't skip it and have mutual delegation beetween 2 models
+      # we get SystemStackError: stack level too deep while trying to load
+      # a chain like user.profile.user.profile.user.profile...
+      next unless send(:association, association).loaded?
+      # skip if association object is nil
+      next unless association_object = send(association)
+      # call private method #changed_attributes
+      association_changed_attributes = association_object.send(:changed_attributes)
+      # next if attribute hasn't been changed
+      next unless association_changed_attributes.has_key?(attribute.to_s)
+
+      result.merge! delegated_attribute => association_changed_attributes[attribute]
+    end
+    changed_attributes = super
+    changed_attributes.merge!(result)
+    changed_attributes
   end
 end
 
